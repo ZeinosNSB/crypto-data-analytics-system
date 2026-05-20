f788d83f56f9942ff027d6a7e1ce4ee5d540c9ab
duy2011-02 <duycoi9c@gmail.com>
2026-05-20 15:19:36 +0700

    thay doi syntaxdo bi loi khi build ingestion

--- ingestion/collector/index.js
+++ ingestion/collector/index.js
@@ -136,7 +136,6 @@ async function runCollector() {
             await new Promise(resolve => setTimeout(resolve, randomizedBackoff));
         }
     }
-  }
 }
 
 // ==========================================
